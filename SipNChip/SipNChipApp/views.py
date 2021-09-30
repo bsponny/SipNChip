eb0a6ec028428e244257058e3608f3b35fc3611a
Gavin Murdock <murdock.gavin1@gmail.com>
2021-09-30 12:46:23 -0600

    added basic login/logout functionality

--- SipNChip/SipNChipApp/views.py
+++ SipNChip/SipNChipApp/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UsernameField
+from django.shortcuts import redirect, render
+from django.contrib import messages
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='SipNChipApp:login')
+def home(request):
+    return render(request, 'SipNChipApp/home.html', {})
+
+def registerPage(request):
+    if request.user.is_authenticated:
+        return redirect('SipNChipApp:home')
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account was created for ' + user)
+            return redirect('SipNChipApp:login')
+
+    context = {'form': form}
+    return render(request, 'SipNChipApp/register.html', context)
+
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('SipNChipApp:home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('SipNChipApp:home')
+        else:
+            messages.info(request, 'Username or password is incorrect.')
+
+    return render(request, 'SipNChipApp/login.html', {})
+
+def logoutUser(request):
+    logout(request)
+    return redirect('SipNChipApp:login')
